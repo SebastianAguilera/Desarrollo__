26781c9578cb10a36e0963dca1b898a06c704de8
JazPC <jazpc04@gmail.com>
2025-05-05 17:58:03 -0300

    añadi el test de orientacion

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,5 @@
 from .facultad import Facultad
-<<<<<<< HEAD
 from .especialidad import Especialidad
-=======
 from .plan import Plan
 from .materia import Materia
->>>>>>> 45ddbb1b5f3e6fd5fe34e6c8bbd5b4a1348866f0
+from .orientacion import Orientacion
