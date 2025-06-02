64a89dfb6f36b5c9d9999467409a64d05d1a7f1a
Rocio Aguilera <rocio.aguilera214@gmail.com>
2025-06-01 21:43:57 -0300

    modelo autoridad

--- app/repositories/autoridad_respositorio.py
+++ app/repositories/autoridad_respositorio.py
@@ -5,7 +5,7 @@ from app.models import Autoridad
 #a parte del CRUD tiene consultas a la base de datos
 
 class AutoridadRepository:
-    def crear(autoridad: db.Model):
+    def crear(autoridad):
         "crea una nnueva autoridad en la base de datos"
         
         db.session.add(autoridad)
--- test/test_autoridad.py
+++ test/test_autoridad.py
@@ -7,7 +7,7 @@ from app.services import AutoridadService
 class CartTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app("testing")
+        self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()  #crea tablas
@@ -22,50 +22,16 @@ class CartTestCase(unittest.TestCase):
         
     def test_autoridad(self):
         autoridad = Autoridad()
-        autoridad.nombre="Dra. Ana Pérez"
+        autoridad.nombre="Dra. Ana Prez"
         autoridad.cargo="Decana"
         autoridad.telefono="123456789"
-        autoridad.email="ana.perez@universidad.edu"
+        autoridad.email="ana.perezuniversidad.edu"
         
-        self.assertEqual(autoridad.nombre, "Dra. Ana Pérez")
+        self.assertEqual(autoridad.nombre, "Dra. Ana Prez")
         self.assertEqual(autoridad.cargo, "Decana")
         self.assertEqual(autoridad.telefono, "123456789")
-        self.assertEqual(autoridad.email, "ana.perez@universidad.edu")
+        self.assertEqual(autoridad.email, "ana.perezuniversidad.edu")
     
-    def test_crear_autoridad(self):
-        
-        autoridad = self.__nuevaautoridad()  
-        
-        AutoridadService.crear_autoridad(autoridad)
-        
-        self.assertIsNotNone(autoridad)
-        self.assertIsNotNone(autoridad.id)
-        self.assertGreaterEqual(autoridad.id,1)
-        self.assertEqual(autoridad.nombre,"Dra. Ana Pérez")
-        
-    def __nuevaautoridad(autoridad):
-        autoridad = Autoridad()
-        autoridad.nombre="Dra. Ana Pérez"
-        autoridad.cargo="Decana"
-        autoridad.telefono="123456789"
-        autoridad.email="ana.perez@universidad.edu" 
-        return autoridad
-    
-    def test_actualizar_autoridad(autoridad):
-        autoridad = self.__nuevaautoridad()  
-        AutoridadService.crear_autoridad(autoridad)
-        autoridad.nombre = ""
-        autoridad_actualizada= AutoridadService.test_actualizar_autoridad(autoridad.nombre,autoridad.cargo,)
-        
-    
-    def test_buscar_autoridad():
-        autoridad = self.__nuevaautoridad()  
-        AutoridadService.crear_autoridad(autoridad)
-        
-    
-    def test_borrar_autoridad():
-        autoridad = self.__nuevaautoridad()  
-        AutoridadService.crear_autoridad(autoridad)
-        
+
 if __name__ == '__main__':
     unittest.main()
