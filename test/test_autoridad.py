a69b4a4ca27d99f27456c458a6af894d1d7d57e6
Rocio Aguilera <rocio.aguilera214@gmail.com>
2025-06-01 23:22:24 -0300

    CRUD Autoridad

--- test/test_autoridad.py
+++ test/test_autoridad.py
@@ -32,6 +32,58 @@ class CartTestCase(unittest.TestCase):
         self.assertEqual(autoridad.telefono, "123456789")
         self.assertEqual(autoridad.email, "ana.perezuniversidad.edu")
     
+    def test_crear_autoridad(self):
+        autoridad = self.__nueva_autoridad()
+        autoridad_guardada = AutoridadService.crear_autoridad(autoridad)
+        
+        self.assertIsNotNone(autoridad_guardada)
+        self.assertIsNotNone(autoridad_guardada.id)
+        self.assertGreaterEqual(autoridad_guardada.id,1)
+        self.assertEqual(autoridad_guardada.nombre, autoridad.nombre)
+        
+    def test_buscar_autoridad_por_id(self):
+        autoridad = self.__nueva_autoridad()
+        AutoridadService.crear_autoridad(autoridad)
+        resultado = AutoridadService.buscar_por_id(autoridad.id)
+        self.assertIsNotNone(resultado)
+        self.assertEqual(resultado.nombre, "Dra. Ana Perez")
+    
+    def test_actualizar_autoridad(self):
+        autoridad = self.__nueva_autoridad()  
+        AutoridadService.crear_autoridad(autoridad)
+
+        autoridad_actualizada = Autoridad()
+        autoridad_actualizada.nombre = "Dra. Belen"
+        autoridad_actualizada.cargo = "Decana"
+        autoridad_actualizada.email = "email"
+        autoridad_actualizada.telefono = "2604567688"
+
+        autoridad_modificada = AutoridadService.actualizar_autoridad(autoridad.id, autoridad_actualizada)
+
+        autoridad_encontrada = AutoridadService.buscar_por_id(autoridad.id)
+        self.assertIsNotNone(autoridad_encontrada)
+        self.assertIsNotNone(autoridad_encontrada.id)
+        self.assertGreaterEqual(autoridad_encontrada.id, 1)
+        self.assertEqual(autoridad_encontrada.nombre, autoridad_modificada.nombre)
+        self.assertEqual(autoridad_encontrada.cargo, autoridad_modificada.cargo)
+        self.assertEqual(autoridad_encontrada.email, autoridad_modificada.email)
+        self.assertEqual(autoridad_encontrada.telefono, autoridad_modificada.telefono)
+
+        
+    def test_borrar_autoridad(self):
+        autoridad = self.__nueva_autoridad()
+        AutoridadService.crear_autoridad(autoridad)
+        AutoridadService.borrar_autoridad(autoridad.id)
+        autoridad_encontrada = AutoridadService.buscar_por_id(autoridad.id)
+        self.assertIsNone(autoridad_encontrada)
+
+    def __nueva_autoridad(self):
+        return Autoridad(
+            nombre="Dra. Ana Perez",
+            cargo="Decana",
+            telefono="123456789",
+            email="ana.perezuniversidad.edu"
+        )
 
 if __name__ == '__main__':
     unittest.main()
