0c678dda99627468824bc7f8c1e30037a087df57
celesc16 <celechoquevillca2004@gmail.com>
2025-06-24 22:05:11 -0300

    Se añade relacion CategoriasCargo a Cargo

--- app/models/cargo.py
+++ app/models/cargo.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
 from app import db
+from sqlalchemy.orm import relationship
 
 @dataclass(init=False, repr=True, eq=True)
 class Cargo(db.Model):
   __tablename__ = 'cargos'
   id: int = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-  nombre: str = db.Column(db.String(100), primary_key=True, nullable=False)
+  nombre: str = db.Column(db.String(100), nullable=False)
   puntos: int = db.Column(db.Integer, nullable=False)
+  categoria_cargo_id: int = db.Column(db.Integer, db.ForeignKey('categorias_cargo.id'), nullable=False)
+  
+  categoria = relationship('CategoriaCargo', back_populates='cargos', lazy='joined')
--- app/models/categoria_cargo.py
+++ app/models/categoria_cargo.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 from app import db
+from sqlalchemy.orm import relationship
 
 @dataclass(init=False, repr=True, eq=True)
 class CategoriaCargo(db.Model):
     __tablename__ = 'categorias_cargo'
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     nombre: str = db.Column(db.String(100), nullable=False)
+    
+    cargos = relationship('Cargo', back_populates='categoria', lazy='select', cascade='all, delete-orphan')    
