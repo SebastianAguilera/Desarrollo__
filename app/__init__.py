436ed724cd28a59d9974a7aa93515334fa34b1a5
Rocio Aguilera <rocio.aguilera214@gmail.com>
2025-08-12 01:21:08 -0300

    modificando el init y arreglando problema en el resource de autoridad

--- app/__init__.py
+++ app/__init__.py
@@ -26,13 +26,15 @@ def create_app() -> Flask:
     ma.init_app(app)
     #jwt.init_app(app)
 
-    from app.resources import home, certificado_bp, universidad_bp, cargo_bp, categoria_cargo_bp, materia_bp
+    from app.resources import home, certificado_bp, universidad_bp, cargo_bp, categoria_cargo_bp, materia_bp, autoridad_bp
     
     app.register_blueprint(home, url_prefix="/api/v1")
     app.register_blueprint(certificado_bp, url_prefix="/api/v1")
     app.register_blueprint(universidad_bp, url_prefix="/api/v1")
     app.register_blueprint(cargo_bp, url_prefix="/api/v1")
     app.register_blueprint(categoria_cargo_bp, url_prefix="/api/v1")
+    app.register_blueprint(autoridad_bp, url_prefix="/api/v1")
+    app.register_blueprint(materia_bp, url_prefix="/api/v1")
 
     @app.shell_context_processor    
     def ctx():
