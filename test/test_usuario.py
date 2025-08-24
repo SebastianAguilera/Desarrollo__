17185388fdc3a48c94d55f51fbcc9e574172d2b9
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2025-08-24 00:46:13 -0300

    se crea test de usuario

--- test/test_usuario.py
+++ test/test_usuario.py
@@ -22,17 +22,46 @@ class AppTestCase(unittest.TestCase):
         self.assertIsNotNone(current_app)
 
     def test_usuario(self):
-        usuario = Usuario()
-        usuario.nombredeusuario = 'alguien'
-        usuario.password = 'alguien.123'
-        usuario.actividad = True
+        usuario = self.__crear_usuario()
+        self.assertIsNotNone(usuario)
+        self.assertEqual(usuario.nombredeusuario, 'alguien')
+        self.assertEqual(usuario.password, 'alguien.123')
+        self.assertEqual(usuario.actividad, True)
 
     def test_crear_usuario(self):
+        usuario = self.__crear_usuario()
+        usuario_guardado = UsuarioService.guardar_usuario(usuario)
+        self.assertIsNotNone(usuario_guardado)
+
+    def test_listar_usuarios(self):
+        usuario = self.__crear_usuario()
+        usuario_guardado = UsuarioService.guardar_usuario(usuario)
+        usuarios = UsuarioService.listar_usuarios()
+        self.assertIsNotNone(usuarios)
+
+    def test_buscar_usuario(self):
+        usuario = self.__crear_usuario()
+        usuario_guardado = UsuarioService.guardar_usuario(usuario)
+        usuario_encontrado = UsuarioService.buscar_usuario(usuario_guardado.id)
+        self.assertIsNotNone(usuario_encontrado)
+        self.assertEqual(usuario_encontrado.id, usuario_guardado.id)
+        self.assertEqual(usuario_encontrado.nombredeusuario, usuario_guardado.nombredeusuario)
+        self.assertEqual(usuario_encontrado.password, usuario_guardado.password)
+        self.assertEqual(usuario_encontrado.actividad, usuario_guardado.actividad)
+
+    def test_eliminar_usuario(self):
+        usuario = self.__crear_usuario()
+        UsuarioService.guardar_usuario(usuario)
+        UsuarioService.eliminar_usuario(usuario.id)
+        usuario_encontrado = UsuarioService.buscar_usuario(usuario.id)
+        self.assertIsNone(usuario_encontrado)
+        
+
+    def __crear_usuario(self):
         usuario = Usuario()
         usuario.nombredeusuario = 'alguien'
         usuario.password = 'alguien.123'
         usuario.actividad = True
-        usuario_guardado = UsuarioService.guardar_usuario(usuario)
-        self.assertIsNotNone(usuario_guardado)
+        return usuario
         
     
