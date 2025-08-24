96003ef6dbbfd389f4b28f550ab1a703b6b7573f
Rocio Aguilera <rocio.aguilera214@gmail.com>
2025-08-24 19:05:57 -0300

    tipo_dedicacion

--- app/mapping/tipo_dedicacion_mapping.py
+++ app/mapping/tipo_dedicacion_mapping.py
@@ -3,8 +3,7 @@ from app.models.tipoDedicacion import TipoDedicacion
 from markupsafe import escape
 
 class Tipo_dedicacionMapping(Schema):
-    hashids = fields.String(attribute='hashid', dump_only=True)
-    id = fields.Integer(dump_only=True) 
+    hashids = fields.String(attribute='hashid', dump_only=True) 
     nombre = fields.String(required=True, validate = validate.Length(min=1, max=100))
     observacion = fields.String(required=True, validate = validate.Length(min=1, max=100))
     
